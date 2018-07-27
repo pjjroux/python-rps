575c4b9c0261d5fde1c7dc79ff838e69252b75fa
Jaco Roux <pjjroux@gmail.com>
2018-07-27 15:33:19 +0200

    Added Match class WIP

---
+++ classes/Match.py
@@ -0,0 +1,90 @@
+class Match(object):
+	""" Represents the Match played over three rounds and controls input and output.
+
+		round_count: And integer tracking round number
+		match_result: A string representing the match winner (Player/COM)
+	"""
+	
+	def __init__(self):
+		""" Return a Match object with a round_count of 0 """
+		self.round_count = 0
+
+	def start_match(self):
+		""" Print welcome message and start first round """
+		self.clear_screen()
+		self.print_start()
+		self.new_round()
+		self.print_user_input()
+
+
+	def new_round(self):
+		self.round_count += 1
+
+	def get_result(self, player_play, com_play):
+		""" Determine outcome of play and return:
+
+		True = Player wins
+		False = COM wins
+		-1    = Draw
+		"""
+
+		rules = {
+			('P','R') : True,
+			('P','S') : False,
+			('R','P') : False,
+			('R','S') : True,
+			('S','P') : True,
+			('S','R') : False
+		}
+
+		if (player_play, com_play) in rules:
+			if rules[(player_play,com_play)]:
+				outcome = 'Player'
+			else:
+				outcome = 'COM'
+
+			result = rules[(player_play,com_play)]
+		else:
+			outcome = 'DRAW'
+			result = -1
+
+		self.print_play(outcome, player_play, com_play)
+		return result
+
+	def print_user_input(self):
+		print()
+		print('Round:')
+		print()
+		print('R: Rock | P: Paper | S: Scissors | X: Exit')
+
+
+	def print_start(self):
+		print('---------------------------------------------------------')
+		print('  Welcome to Super Ultimate Battle Rock Paper Scissors!')
+		print('---------------------------------------------------------')
+
+	def print_play(self, outcome, player_play, com_play):
+		print()
+		self.print_art(player_play, com_play)
+		print()
+		print('Winner: ' + outcome)
+		print()
+		print('---------------------------------------------------------')
+	
+	def print_art(self, player_play, com_play):
+		print('          PLAYER                         COMPUTER')
+
+		file_name = player_play + com_play + '.txt'
+		file = open('resources/' + file_name, 'r')
+		art = file.readlines()
+
+		for line in art:
+			print(line.rstrip('\n'))
+	
+	def clear_screen(self):
+		import os
+
+		if (os.name == 'nt'):
+			os.system('cls')
+		else:
+			os.system('clear')
--- main.py
+++ main.py
@@ -1,31 +1,27 @@
+from classes.Match import Match
 from classes.Player import Player
 from classes.COM import COM
 
+match = Match()
 player = Player()
 com = COM()
 
 try:
-    player_play = player.get_play(input('Rock, paper or scissor? ').upper())
+    match_stop = False
+    match.start_match()
+
+    player_input = input().upper()
+
+    player_play = player.get_play(player_input)
     com_play = com.get_play()
 
-    rules = {
-        ('P','R') : True,
-        ('P','S') : False,
-        ('R','P') : False,
-        ('R','S') : True,
-        ('S','P') : True,
-        ('S','R') : False
-    }
+    result = match.get_result(player_play,com_play)
 
-    if (player_play, com_play) in rules:
-        if rules[(player_play,com_play)]:
-            player.win()
-            print('Player wins! Score: ' + str(player.score))
-        else:
-            com.win()
-            print('COM wins! Score: ' + str(com.score))     
-    else:
-        print('DRAW!')
-except Exception as error:
-    print(error)
+    if result:
+        player.win()
+    elif not result:
+        com.win()
     
+    input()
+except Exception as error:
+    print(error)   
